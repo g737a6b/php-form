b96aa75ef9e1d974fa411e9a619fd3d63c6f2420
Hiroyuki Suzuki <github@mofg.net>
2024-01-01 17:52:44 +0900

    Do fix

--- src/Mail.php
+++ src/Mail.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace MofgForm;
 
 /**
@@ -8,176 +9,210 @@ namespace MofgForm;
  * @author Hiroyuki Suzuki
  * @copyright Copyright (c) 2021 Hiroyuki Suzuki mofg.net
  */
-class Mail{
-	use MultiMethodNameTrait;
+class Mail
+{
+    use MultiMethodNameTrait;
 
-	/**
-	 * @var string
-	 */
-	private $to = "";
+    /**
+     * @var string
+     */
+    private $to = "";
 
-	/**
-	 * @var string
-	 */
-	private $subject = "";
+    /**
+     * @var string
+     */
+    private $subject = "";
 
-	/**
-	 * @var string
-	 */
-	private $body = "";
+    /**
+     * @var string
+     */
+    private $body = "";
 
-	/**
-	 * @var string
-	 */
-	private $header = "";
+    /**
+     * @var string
+     */
+    private $header = "";
 
-	const FORMAT_ADDRESS = '#\A[a-zA-Z0-9.!\#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9_\-]+(\.[a-zA-Z0-9_\-]+)+\z#';
-	const FORMAT_HEADER = '#\A[a-zA-Z0-9_\-]+ *: *[^:]+\z#';
+    public const FORMAT_ADDRESS = '#\A[a-zA-Z0-9.!\#$%&\'*+/=?^_`{|}~-]+@[a-zA-Z0-9_\-]+(\.[a-zA-Z0-9_\-]+)+\z#';
+    public const FORMAT_HEADER = '#\A[a-zA-Z0-9_\-]+ *: *[^:]+\z#';
 
-	/**
-	 * @param string $to (optional)
-	 * @param string $subject (optional)
-	 * @param string $body (optional)
-	 * @param string $header (optional)
-	 * @return boolean
-	 */
-	public function send($to = null, $subject = null, $body = null, $header = null){
-		if( !isset($to) ){
-			$to = $this->to;
-			$subject = $this->subject;
-			$body = $this->body;
-			$header = $this->header;
-		}else if( !isset($subject) && !isset($body) ){
-			return false;
-		}
+    /**
+     * @param string $to (optional)
+     * @param string $subject (optional)
+     * @param string $body (optional)
+     * @param string $header (optional)
+     * @return boolean
+     */
+    public function send($to = null, $subject = null, $body = null, $header = null)
+    {
+        if(!isset($to)) {
+            $to = $this->to;
+            $subject = $this->subject;
+            $body = $this->body;
+            $header = $this->header;
+        } elseif(!isset($subject) && !isset($body)) {
+            return false;
+        }
 
-		$result = false;
-		$to = $this->construct_data($to, ",", self::FORMAT_ADDRESS);
-		if( !empty($to) ){
-			$header = $this->construct_data($header, "\n", self::FORMAT_HEADER);
-			if( empty($header) ){
-				$result = mb_send_mail($to, $subject, $body);
-			}else{
-				$header = $this->group_header($header);
-				$result = mb_send_mail($to, $subject, $body, $header);
-			}
-		}
-		return $result;
-	}
+        $result = false;
+        $to = $this->construct_data($to, ",", self::FORMAT_ADDRESS);
+        if(!empty($to)) {
+            $header = $this->construct_data($header, "\n", self::FORMAT_HEADER);
+            if(empty($header)) {
+                $result = mb_send_mail($to, $subject, $body);
+            } else {
+                $header = $this->group_header($header);
+                $result = mb_send_mail($to, $subject, $body, $header);
+            }
+        }
+        return $result;
+    }
 
-	/**
-	 * @param string|array $data
-	 * @param string $separator (optional)
-	 * @param string $pattern (optional)
-	 * @return string
-	 */
-	public function construct_data($data, $separator = ",", $pattern = '/.*/'){
-		if( is_string($data) ){
-			$data = trim($data, " \t\n\r\0\x0B");
-			$data = explode($separator, $data);
-		}
-		if( !is_array($data) ) return false;
+    /**
+     * @param string|array $data
+     * @param string $separator (optional)
+     * @param string $pattern (optional)
+     * @return string
+     */
+    public function construct_data($data, $separator = ",", $pattern = '/.*/')
+    {
+        if(is_string($data)) {
+            $data = trim($data, " \t\n\r\0\x0B");
+            $data = explode($separator, $data);
+        }
+        if(!is_array($data)) {
+            return false;
+        }
 
-		$count = count($data);
-		for($i = 0; $i < $count; $i++){
-			if( is_string($data[$i]) ){
-				$data[$i] = trim($data[$i], " \t\n\r\0\x0B");
-				if( !preg_match($pattern, $data[$i]) ) unset($data[$i]);
-			}else{
-				unset($data[$i]);
-			}
-		}
-		return implode($separator, $data);
-	}
+        $count = count($data);
+        for($i = 0; $i < $count; $i++) {
+            if(is_string($data[$i])) {
+                $data[$i] = trim($data[$i], " \t\n\r\0\x0B");
+                if(!preg_match($pattern, $data[$i])) {
+                    unset($data[$i]);
+                }
+            } else {
+                unset($data[$i]);
+            }
+        }
+        return implode($separator, $data);
+    }
 
-	/**
-	 * @param string $header
-	 * @return string
-	 */
-	public function group_header($header){
-		if( !is_string($header) ) return false;
-		$result = "";
-		$groups = [];
+    /**
+     * @param string $header
+     * @return string
+     */
+    public function group_header($header)
+    {
+        if(!is_string($header)) {
+            return false;
+        }
+        $result = "";
+        $groups = [];
 
-		$items = explode("\n", $header);
-		foreach($items as $i){
-			list($k, $v) = explode(":", $i);
-			$k = trim($k, " \t\n\r\0\x0B");
-			$v = trim($v, " \t\n\r\0\x0B");
-			if( !isset($groups[$k]) ) $groups[$k] = [];
-			$groups[$k][] = $v;
-		}
+        $items = explode("\n", $header);
+        foreach($items as $i) {
+            list($k, $v) = explode(":", $i);
+            $k = trim($k, " \t\n\r\0\x0B");
+            $v = trim($v, " \t\n\r\0\x0B");
+            if(!isset($groups[$k])) {
+                $groups[$k] = [];
+            }
+            $groups[$k][] = $v;
+        }
 
-		foreach($groups as $k => $v){
-			if( $result !== "" ) $result .= "\n";
-			$result .= $k.": ".implode(", ", $v);
-		}
-		return $result;
-	}
+        foreach($groups as $k => $v) {
+            if($result !== "") {
+                $result .= "\n";
+            }
+            $result .= $k.": ".implode(", ", $v);
+        }
+        return $result;
+    }
 
-	/**
-	 * @param string|array $to
-	 */
-	public function add_to($to){
-		$add = $this->construct_data($to, ",", self::FORMAT_ADDRESS);
-		if( $add === false ) return;
-		if( !empty($this->to) && $add !== "" ) $this->to .= ",";
-		$this->to .= $add;
-	}
+    /**
+     * @param string|array $to
+     */
+    public function add_to($to)
+    {
+        $add = $this->construct_data($to, ",", self::FORMAT_ADDRESS);
+        if($add === false) {
+            return;
+        }
+        if(!empty($this->to) && $add !== "") {
+            $this->to .= ",";
+        }
+        $this->to .= $add;
+    }
 
-	/**
-	 * @param string $subject
-	 */
-	public function set_subject($subject){
-		if( !is_string($subject) ) return;
-		$this->subject = $subject;
-	}
+    /**
+     * @param string $subject
+     */
+    public function set_subject($subject)
+    {
+        if(!is_string($subject)) {
+            return;
+        }
+        $this->subject = $subject;
+    }
 
-	/**
-	 * @param string $body
-	 */
-	public function set_body($body){
-		if( !is_string($body) ) return;
-		$this->body = $body;
-	}
+    /**
+     * @param string $body
+     */
+    public function set_body($body)
+    {
+        if(!is_string($body)) {
+            return;
+        }
+        $this->body = $body;
+    }
 
-	/**
-	 * @param string|array $header
-	 */
-	public function add_header($header){
-		$add = $this->construct_data($header, "\n", self::FORMAT_HEADER);
-		if( $add === false ) return;
-		if( !empty($this->header) && $add !== "" ) $this->header .= "\n";
-		$this->header .= $add;
-	}
+    /**
+     * @param string|array $header
+     */
+    public function add_header($header)
+    {
+        $add = $this->construct_data($header, "\n", self::FORMAT_HEADER);
+        if($add === false) {
+            return;
+        }
+        if(!empty($this->header) && $add !== "") {
+            $this->header .= "\n";
+        }
+        $this->header .= $add;
+    }
 
-	/**
-	 * @param boolean $array (optional)
-	 * @return mixed
-	 */
-	public function get_to($array = false){
-		return ( $array ) ? explode(",", $this->to) : $this->to;
-	}
+    /**
+     * @param boolean $array (optional)
+     * @return mixed
+     */
+    public function get_to($array = false)
+    {
+        return ($array) ? explode(",", $this->to) : $this->to;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function get_subject(){
-		return $this->subject;
-	}
+    /**
+     * @return string
+     */
+    public function get_subject()
+    {
+        return $this->subject;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function get_body(){
-		return $this->body;
-	}
+    /**
+     * @return string
+     */
+    public function get_body()
+    {
+        return $this->body;
+    }
 
-	/**
-	 * @param boolean $array (optional)
-	 * @return mixed
-	 */
-	public function get_header($array = false){
-		return ( $array ) ? explode("\n", $this->header) : $this->header;
-	}
+    /**
+     * @param boolean $array (optional)
+     * @return mixed
+     */
+    public function get_header($array = false)
+    {
+        return ($array) ? explode("\n", $this->header) : $this->header;
+    }
 }
--- tests/HtmlTest.php
+++ tests/HtmlTest.php
@@ -1,275 +1,291 @@
 <?php
+
 require(__DIR__."/../autoload.php");
 
 use PHPUnit\Framework\TestCase;
 use MofgForm\Html;
 
-class HtmlTest extends TestCase{
-	protected $Form;
+class HtmlTest extends TestCase
+{
+    protected $Form;
 
-	protected function setUp() : void{
-		$this->Form = $this->createMock(MofgForm\MofgForm::class);
-		$this->Form->method("get_value")->willReturn("foo");
-	}
+    protected function setUp(): void
+    {
+        $this->Form = $this->createMock(MofgForm\MofgForm::class);
+        $this->Form->method("get_value")->willReturn("foo");
+    }
 
-	/**
-	 * @dataProvider provider_for_test_checkbox
-	 */
-	public function test_checkbox($expected, $name, $items, $attrs){
-		$Html = new MofgForm\Html($this->Form);
-		$this->expectOutputString($expected);
-		$Html->checkbox($name, $items, $attrs);
-	}
+    /**
+     * @dataProvider provider_for_test_checkbox
+     */
+    public function test_checkbox($expected, $name, $items, $attrs)
+    {
+        $Html = new MofgForm\Html($this->Form);
+        $this->expectOutputString($expected);
+        $Html->checkbox($name, $items, $attrs);
+    }
 
-	public static function provider_for_test_checkbox(){
-		return [
-			[
-				"",
-				"test",
-				[],
-				[]
-			],
-			[
-				"<label><input type=\"checkbox\" name=\"test\" value=\"bar\" /> bar</label>",
-				"test",
-				["bar"],
-				[]
-			],
-			[
-				"<label><input type=\"checkbox\" name=\"test\" value=\"foo\" checked=\"checked\" /> foo</label>",
-				"test",
-				["foo"],
-				[]
-			],
-			[
-				"<label><input type=\"checkbox\" name=\"test[]\" value=\"bar\" /> bar</label><label><input type=\"checkbox\" name=\"test[]\" value=\"baz\" /> baz</label>",
-				"test",
-				["bar", "baz"],
-				[]
-			],
-			[
-				"<label><input type=\"checkbox\" name=\"test[]\" value=\"foo\" checked=\"checked\" /> foo</label><label><input type=\"checkbox\" name=\"test[]\" value=\"bar\" /> bar</label>",
-				"test",
-				["foo", "bar"],
-				[]
-			],
-			[
-				"<label><input type=\"checkbox\" name=\"test[]\" value=\"foo\" checked=\"checked\" /> Apple</label><label><input type=\"checkbox\" name=\"test[]\" value=\"bar\" /> Orange</label>",
-				"test",
-				["foo" => "Apple", "bar" => "Orange"],
-				[]
-			],
-			[
-				"<label class=\"colored\"><input type=\"checkbox\" name=\"test\" value=\"bar\" /> bar</label>",
-				"test",
-				["bar"],
-				["class" => "colored"]
-			]
-		];
-	}
+    public static function provider_for_test_checkbox()
+    {
+        return [
+            [
+                "",
+                "test",
+                [],
+                []
+            ],
+            [
+                "<label><input type=\"checkbox\" name=\"test\" value=\"bar\" /> bar</label>",
+                "test",
+                ["bar"],
+                []
+            ],
+            [
+                "<label><input type=\"checkbox\" name=\"test\" value=\"foo\" checked=\"checked\" /> foo</label>",
+                "test",
+                ["foo"],
+                []
+            ],
+            [
+                "<label><input type=\"checkbox\" name=\"test[]\" value=\"bar\" /> bar</label><label><input type=\"checkbox\" name=\"test[]\" value=\"baz\" /> baz</label>",
+                "test",
+                ["bar", "baz"],
+                []
+            ],
+            [
+                "<label><input type=\"checkbox\" name=\"test[]\" value=\"foo\" checked=\"checked\" /> foo</label><label><input type=\"checkbox\" name=\"test[]\" value=\"bar\" /> bar</label>",
+                "test",
+                ["foo", "bar"],
+                []
+            ],
+            [
+                "<label><input type=\"checkbox\" name=\"test[]\" value=\"foo\" checked=\"checked\" /> Apple</label><label><input type=\"checkbox\" name=\"test[]\" value=\"bar\" /> Orange</label>",
+                "test",
+                ["foo" => "Apple", "bar" => "Orange"],
+                []
+            ],
+            [
+                "<label class=\"colored\"><input type=\"checkbox\" name=\"test\" value=\"bar\" /> bar</label>",
+                "test",
+                ["bar"],
+                ["class" => "colored"]
+            ]
+        ];
+    }
 
-	/**
-	 * @dataProvider provider_for_test_radio
-	 */
-	public function test_radio($expected, $name, $items, $attrs){
-		$Html = new MofgForm\Html($this->Form);
-		$this->expectOutputString($expected);
-		$Html->radio($name, $items, $attrs);
-	}
+    /**
+     * @dataProvider provider_for_test_radio
+     */
+    public function test_radio($expected, $name, $items, $attrs)
+    {
+        $Html = new MofgForm\Html($this->Form);
+        $this->expectOutputString($expected);
+        $Html->radio($name, $items, $attrs);
+    }
 
-	public static function provider_for_test_radio(){
-		return [
-			[
-				"",
-				"test",
-				[],
-				[]
-			],
-			[
-				"<label><input type=\"radio\" name=\"test\" value=\"bar\" /> bar</label>",
-				"test",
-				["bar"],
-				[]
-			],
-			[
-				"<label><input type=\"radio\" name=\"test\" value=\"foo\" checked=\"checked\" /> foo</label>",
-				"test",
-				["foo"],
-				[]
-			],
-			[
-				"<label><input type=\"radio\" name=\"test\" value=\"bar\" /> bar</label><label><input type=\"radio\" name=\"test\" value=\"baz\" /> baz</label>",
-				"test",
-				["bar", "baz"],
-				[]
-			],
-			[
-				"<label><input type=\"radio\" name=\"test\" value=\"foo\" checked=\"checked\" /> foo</label><label><input type=\"radio\" name=\"test\" value=\"bar\" /> bar</label>",
-				"test",
-				["foo", "bar"],
-				[]
-			],
-			[
-				"<label><input type=\"radio\" name=\"test\" value=\"foo\" checked=\"checked\" /> Apple</label><label><input type=\"radio\" name=\"test\" value=\"bar\" /> Orange</label>",
-				"test",
-				["foo" => "Apple", "bar" => "Orange"],
-				[]
-			],
-			[
-				"<label class=\"colored\"><input type=\"radio\" name=\"test\" value=\"bar\" /> bar</label>",
-				"test",
-				["bar"],
-				["class" => "colored"]
-			]
-		];
-	}
+    public static function provider_for_test_radio()
+    {
+        return [
+            [
+                "",
+                "test",
+                [],
+                []
+            ],
+            [
+                "<label><input type=\"radio\" name=\"test\" value=\"bar\" /> bar</label>",
+                "test",
+                ["bar"],
+                []
+            ],
+            [
+                "<label><input type=\"radio\" name=\"test\" value=\"foo\" checked=\"checked\" /> foo</label>",
+                "test",
+                ["foo"],
+                []
+            ],
+            [
+                "<label><input type=\"radio\" name=\"test\" value=\"bar\" /> bar</label><label><input type=\"radio\" name=\"test\" value=\"baz\" /> baz</label>",
+                "test",
+                ["bar", "baz"],
+                []
+            ],
+            [
+                "<label><input type=\"radio\" name=\"test\" value=\"foo\" checked=\"checked\" /> foo</label><label><input type=\"radio\" name=\"test\" value=\"bar\" /> bar</label>",
+                "test",
+                ["foo", "bar"],
+                []
+            ],
+            [
+                "<label><input type=\"radio\" name=\"test\" value=\"foo\" checked=\"checked\" /> Apple</label><label><input type=\"radio\" name=\"test\" value=\"bar\" /> Orange</label>",
+                "test",
+                ["foo" => "Apple", "bar" => "Orange"],
+                []
+            ],
+            [
+                "<label class=\"colored\"><input type=\"radio\" name=\"test\" value=\"bar\" /> bar</label>",
+                "test",
+                ["bar"],
+                ["class" => "colored"]
+            ]
+        ];
+    }
 
-	/**
-	 * @dataProvider provider_for_test_select
-	 */
-	public function test_select($expected, $name, $options, $empty, $attrs){
-		$Html = new MofgForm\Html($this->Form);
-		$this->expectOutputString($expected);
-		$Html->select($name, $options, $empty, $attrs);
-	}
+    /**
+     * @dataProvider provider_for_test_select
+     */
+    public function test_select($expected, $name, $options, $empty, $attrs)
+    {
+        $Html = new MofgForm\Html($this->Form);
+        $this->expectOutputString($expected);
+        $Html->select($name, $options, $empty, $attrs);
+    }
 
-	public static function provider_for_test_select(){
-		return [
-			[
-				"<select name=\"test\"></select>",
-				"test",
-				[],
-				"",
-				[]
-			],
-			[
-				"<select name=\"test\"><option value=\"bar\">bar</option></select>",
-				"test",
-				["bar"],
-				"",
-				[]
-			],
-			[
-				"<select name=\"test\"><option value=\"foo\" selected=\"selected\">foo</option></select>",
-				"test",
-				["foo"],
-				"",
-				[]
-			],
-			[
-				"<select name=\"test\"><option value=\"bar\">bar</option><option value=\"baz\">baz</option></select>",
-				"test",
-				["bar", "baz"],
-				"",
-				[]
-			],
-			[
-				"<select name=\"test\"><option value=\"foo\" selected=\"selected\">foo</option><option value=\"bar\">bar</option></select>",
-				"test",
-				["foo", "bar"],
-				"",
-				[]
-			],
-			[
-				"<select name=\"test\"><option value=\"\">----</option></select>",
-				"test",
-				[],
-				"----",
-				[]
-			],
-			[
-				"<select name=\"test\"><option value=\"\">----</option><option value=\"bar\">bar</option></select>",
-				"test",
-				["bar"],
-				"----",
-				[]
-			],
-			[
-				"<select name=\"test\" class=\"colored\"></select>",
-				"test",
-				[],
-				"",
-				["class" => "colored"]
-			]
-		];
-	}
+    public static function provider_for_test_select()
+    {
+        return [
+            [
+                "<select name=\"test\"></select>",
+                "test",
+                [],
+                "",
+                []
+            ],
+            [
+                "<select name=\"test\"><option value=\"bar\">bar</option></select>",
+                "test",
+                ["bar"],
+                "",
+                []
+            ],
+            [
+                "<select name=\"test\"><option value=\"foo\" selected=\"selected\">foo</option></select>",
+                "test",
+                ["foo"],
+                "",
+                []
+            ],
+            [
+                "<select name=\"test\"><option value=\"bar\">bar</option><option value=\"baz\">baz</option></select>",
+                "test",
+                ["bar", "baz"],
+                "",
+                []
+            ],
+            [
+                "<select name=\"test\"><option value=\"foo\" selected=\"selected\">foo</option><option value=\"bar\">bar</option></select>",
+                "test",
+                ["foo", "bar"],
+                "",
+                []
+            ],
+            [
+                "<select name=\"test\"><option value=\"\">----</option></select>",
+                "test",
+                [],
+                "----",
+                []
+            ],
+            [
+                "<select name=\"test\"><option value=\"\">----</option><option value=\"bar\">bar</option></select>",
+                "test",
+                ["bar"],
+                "----",
+                []
+            ],
+            [
+                "<select name=\"test\" class=\"colored\"></select>",
+                "test",
+                [],
+                "",
+                ["class" => "colored"]
+            ]
+        ];
+    }
 
-	/**
-	 * @dataProvider provider_for_test_text
-	 */
-	public function test_text($expected, $name, $attrs){
-		$Html = new MofgForm\Html($this->Form);
-		$this->expectOutputString($expected);
-		$Html->text($name, $attrs);
-	}
+    /**
+     * @dataProvider provider_for_test_text
+     */
+    public function test_text($expected, $name, $attrs)
+    {
+        $Html = new MofgForm\Html($this->Form);
+        $this->expectOutputString($expected);
+        $Html->text($name, $attrs);
+    }
 
-	public static function provider_for_test_text(){
-		return [
-			[
-				"<input type=\"text\" name=\"test\" value=\"foo\" />",
-				"test",
-				[]
-			],
-			[
-				"<input type=\"text\" name=\"test\" value=\"foo\" class=\"colored\" />",
-				"test",
-				["class" => "colored"]
-			]
-		];
-	}
+    public static function provider_for_test_text()
+    {
+        return [
+            [
+                "<input type=\"text\" name=\"test\" value=\"foo\" />",
+                "test",
+                []
+            ],
+            [
+                "<input type=\"text\" name=\"test\" value=\"foo\" class=\"colored\" />",
+                "test",
+                ["class" => "colored"]
+            ]
+        ];
+    }
 
-	/**
-	 * @dataProvider provider_for_test_password
-	 */
-	public function test_password($expected, $name, $attrs){
-		$Html = new MofgForm\Html($this->Form);
-		$this->expectOutputString($expected);
-		$Html->password($name, $attrs);
-	}
+    /**
+     * @dataProvider provider_for_test_password
+     */
+    public function test_password($expected, $name, $attrs)
+    {
+        $Html = new MofgForm\Html($this->Form);
+        $this->expectOutputString($expected);
+        $Html->password($name, $attrs);
+    }
 
-	public static function provider_for_test_password(){
-		return [
-			[
-				"<input type=\"password\" name=\"test\" value=\"foo\" />",
-				"test",
-				[]
-			],
-			[
-				"<input type=\"password\" name=\"test\" value=\"foo\" class=\"colored\" />",
-				"test",
-				["class" => "colored"]
-			]
-		];
-	}
+    public static function provider_for_test_password()
+    {
+        return [
+            [
+                "<input type=\"password\" name=\"test\" value=\"foo\" />",
+                "test",
+                []
+            ],
+            [
+                "<input type=\"password\" name=\"test\" value=\"foo\" class=\"colored\" />",
+                "test",
+                ["class" => "colored"]
+            ]
+        ];
+    }
 
-	/**
-	 * @dataProvider provider_for_test_textarea
-	 */
-	public function test_textarea($expected, $name, $attrs){
-		$Html = new MofgForm\Html($this->Form);
-		$this->expectOutputString($expected);
-		$Html->textarea($name, $attrs);
-	}
+    /**
+     * @dataProvider provider_for_test_textarea
+     */
+    public function test_textarea($expected, $name, $attrs)
+    {
+        $Html = new MofgForm\Html($this->Form);
+        $this->expectOutputString($expected);
+        $Html->textarea($name, $attrs);
+    }
 
-	public static function provider_for_test_textarea(){
-		return [
-			[
-				"<textarea name=\"test\">foo</textarea>",
-				"test",
-				[]
-			],
-			[
-				"<textarea name=\"test\" class=\"colored\">foo</textarea>",
-				"test",
-				["class" => "colored"]
-			]
-		];
-	}
+    public static function provider_for_test_textarea()
+    {
+        return [
+            [
+                "<textarea name=\"test\">foo</textarea>",
+                "test",
+                []
+            ],
+            [
+                "<textarea name=\"test\" class=\"colored\">foo</textarea>",
+                "test",
+                ["class" => "colored"]
+            ]
+        ];
+    }
 
-	public function test_get_attr_text(){
-		$Html = new MofgForm\Html($this->Form);
-		$this->assertSame("", $Html->get_attr_text([]));
-		$this->assertSame(" class=\"colored\"", $Html->get_attr_text(["class" => "colored"]));
-		$this->assertSame(" class=\"colored\" data-id=\"100\"", $Html->get_attr_text(["class" => "colored", "data-id" => "100"]));
-	}
+    public function test_get_attr_text()
+    {
+        $Html = new MofgForm\Html($this->Form);
+        $this->assertSame("", $Html->get_attr_text([]));
+        $this->assertSame(" class=\"colored\"", $Html->get_attr_text(["class" => "colored"]));
+        $this->assertSame(" class=\"colored\" data-id=\"100\"", $Html->get_attr_text(["class" => "colored", "data-id" => "100"]));
+    }
 }
